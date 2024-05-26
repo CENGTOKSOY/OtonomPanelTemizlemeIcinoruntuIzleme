9c96f0f92fca340b506bf274649ab58214b2a958
Ali Gaffar Toksoy <126244238+CENGTOKSOY@users.noreply.github.com>
2024-05-26 23:54:31 +0300

    Update SolarPanelCleanRobot.py

--- SolarPanelCleanRobot.py
+++ SolarPanelCleanRobot.py
@@ -42,6 +42,25 @@ class Robot:
             self.stop_all_motors()
 
     
+    def set_motor_direction(self, direction):
+        if direction == "ileri":
+            GPIO.output(self.motor_pins["DC Motor 1 IN1"], GPIO.HIGH)
+            GPIO.output(self.motor_pins["DC Motor 1 IN2"], GPIO.LOW)
+            GPIO.output(self.motor_pins["DC Motor 2 IN1"], GPIO.HIGH)
+            GPIO.output(self.motor_pins["DC Motor 2 IN2"], GPIO.LOW)
+            GPIO.output(self.motor_pins["DC Motor 3 IN1"], GPIO.HIGH)
+            GPIO.output(self.motor_pins["DC Motor 3 IN2"], GPIO.LOW)
+            GPIO.output(self.motor_pins["DC Motor 4 IN1"], GPIO.HIGH)
+            GPIO.output(self.motor_pins["DC Motor 4 IN2"], GPIO.LOW)
+        elif direction == "geri":
+            GPIO.output(self.motor_pins["DC Motor 1 IN1"], GPIO.LOW)
+            GPIO.output(self.motor_pins["DC Motor 1 IN2"], GPIO.HIGH)
+            GPIO.output(self.motor_pins["DC Motor 2 IN1"], GPIO.LOW)
+            GPIO.output(self.motor_pins["DC Motor 2 IN2"], GPIO.HIGH)
+            GPIO.output(self.motor_pins["DC Motor 3 IN1"], GPIO.LOW)
+            GPIO.output(self.motor_pins["DC Motor 3 IN2"], GPIO.HIGH)
+            GPIO.output(self.motor_pins["DC Motor 4 IN1"], GPIO.LOW)
+            GPIO.output(self.motor_pins["DC Motor 4 IN2"], GPIO.HIGH)
 
     def activate_motors(self, motors):
         for motor in motors:
